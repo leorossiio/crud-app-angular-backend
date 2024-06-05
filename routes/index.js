2f4b1575969e55e326029a05da14ec4d5e125c73
Leonardo Rossi <leonardo.oliveira@itixti.com.br>
2024-06-04 23:49:56 -0300

    inclui as rotas do index.js para login e user

--- routes/index.js
+++ routes/index.js
@@ -3,40 +3,38 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const loginController = require("../controllers/loginController");
-const userController = require("../controllers/userController");
-const todoListController = require("../controllers/todoListController");
+const userController = require("../Controllers/autenticacao/userController");
 
-dotenv.config(); // Carrega as variáveis de ambiente do arquivo .env
+// Carrega as variáveis de ambiente do arquivo .env
+dotenv.config();
 
 const servidor = express();
+
+// Middleware para processar o corpo das solicitações como JSON
 servidor.use(express.json());
+
+// Middleware para permitir solicitações de outros domínios (CORS)
 servidor.use(cors());
 
+// Define as rotas para os controladores de login e usuário
+servidor.use("/login", loginController);
+servidor.use("/users", userController);
+
+// Conexão com o banco de dados MongoDB
 const PORT = process.env.PORT;
 const DB_NAME = process.env.DB_NAME;
 const DB_USER = process.env.DB_USER;
 const DB_PASS = process.env.DB_PASS;
-const DB_URL = `mongodb+srv://${DB_USER}:${DB_PASS}@crud-app.yso2wfp.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
-
-// servidor.use("/login", loginController);
-// servidor.use("/users", userController);
-// servidor.use("/todoList", todoListController);
+const DB_URL = `mongodb+srv://${DB_USER}:${DB_PASS}@crud-app.yso2wfp.mongodb.net/${DB_NAME}`;
 
 mongoose
   .connect(DB_URL)
   .then(() => {
     console.log("Banco de dados conectado com sucesso");
     servidor.listen(PORT, () => {
-      console.log(`servidor rodando na porta ${PORT}`);
+      console.log(`Servidor rodando na porta ${PORT}`);
     });
   })
   .catch((error) => {
     console.log(`Erro ao conectar no banco de dados. ${error}`);
   });
-
-servidor.get("/", (req, res) => {
-  res.send("Metodo Get"); 
-});
-servidor.post("/", (req, res) => {
-  res.send("Metodo post"); 
-});
