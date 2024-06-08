ccf3da89ea951e34d7db8e5958b2097d13b9bbb1
Leonardo Rossi <leonardo.oliveira@itixti.com.br>
2024-06-07 23:59:38 -0300

    ajuste de identação

--- Controllers/loginController.js
+++ Controllers/loginController.js
@@ -1,26 +1,29 @@
-const UserModel = require("../models/User")
-const bcryptjs = require('bcryptjs')
-const jwt = require('jsonwebtoken')
-
+const UserModel = require("../models/User");
+const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const loginController = async (req, res) => {
-    const { email, senha } = req.body
+  const { email, senha } = req.body;
 
-    var user = await UserModel.findOne({email: email})
-    if(!user) {
-        return res.status(400).json({mensagem: "Usuário não encontrado"})
-    }
+  var user = await UserModel.findOne({ email: email });
+  if (!user) {
+    return res.status(400).json({ mensagem: "Usuário não encontrado" });
+  }
 
-    if(await bcryptjs.compare(senha, user.senha)) {
-        const token = jwt.sign({ nome: user.nome, email: user.email}, process.env.JWT_SECRET, { expiresIn: '2d' })
+  if (await bcryptjs.compare(senha, user.senha)) {
+    const token = jwt.sign(
+      { nome: user.nome, email: user.email },
+      process.env.JWT_SECRET,
+      { expiresIn: "2d" }
+    );
 
-        return res.status(200).json({
-            mensagem: "Usuario logado com sucesso!",
-            token: token
-        })
-    } else {
-        return res.status(401).json({mensagem: "Email ou senha inválidos"})
-    }
-}
+    return res.status(200).json({
+      mensagem: "Usuario logado com sucesso!",
+      token: token,
+    });
+  } else {
+    return res.status(401).json({ mensagem: "Email ou senha inválidos" });
+  }
+};
 
 module.exports = loginController;
--- models/User.js
+++ models/User.js
@@ -1,10 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const UserModel = mongoose.model('users', {
-    nome: String,
-    email: String,
-    senha: String,
-    funcao: String
-})
+const UserModel = mongoose.model("users", {
+  nome: String,
+  email: String,
+  senha: String,
+  funcao: String,
+});
 
-module.exports = UserModel
+module.exports = UserModel;
