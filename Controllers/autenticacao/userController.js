edbd2e2d56b2e46a859e6670c69d2efaa63e0cda
Leonardo Rossi <leonardo.oliveira@itixti.com.br>
2024-06-07 23:20:20 -0300

    cria as rotas autenticadas: cadastroUsuario e deleteUsuario

--- Controllers/autenticacao/userController.js
+++ Controllers/autenticacao/userController.js
@@ -1,78 +1,164 @@
-const bcryptjs = require('bcryptjs');
-const autenticado = require("../../middlewares/authentication")
-const usuarioModel = require("../../models/User");
-const express = require('express');
+const bcryptjs = require("bcryptjs");
+const auth = require("../../middlewares/authentication"); // Importe o middleware de autenticação corretamente
+const UserModel = require("../../models/User");
+const express = require("express");
 const userController = express.Router();
 
-//Rotas não autenticadas:
+// Rotas não autenticadas:
 
 // Rota para criar um novo usuário/cliente
-userController.post("/cadastroCliente", async (req, res) => {
-    const { nome, email, senha } = req.body;
+userController.post("/cadastroUsuarioNaoAutenticada", async (req, res) => {
+  const { nome, email, senha } = req.body;
 
-    // Verificar se o nome de usuário ou email já existe
-    const usuarioExistente = await usuarioModel.findOne({ $or: [{ nome: nome }, { email: email }] });
+  // Verificar se o nome de usuário ou email já existe
+  const usuarioExistente = await UserModel.findOne({
+    $or: [{ nome: nome }, { email: email }],
+  });
+  if (usuarioExistente) {
+    return res.status(400).json({
+      mensagem: "Nome de usuário ou email já existe!",
+    });
+  }
+
+  const senhaEncrypt = await bcryptjs.hash(senha, 10);
+  var user = {
+    nome: nome,
+    email: email,
+    senha: senhaEncrypt,
+    funcao: null,
+  };
+
+  try {
+    await UserModel.create(user);
+    return res.status(201).json({
+      mensagem: "Usuário criado com sucesso!",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: error,
+    });
+  }
+});
+
+// Rotas autenticadas:
+
+// Rota para obter um usuário pelo email
+userController.get("/:email", auth, async (req, res) => {
+  var email = req.params.email;
+
+  try {
+    let user = await UserModel.findOne({ email: email });
+    if (!user) {
+      return res
+        .status(404)
+        .json({ mensagem: "Usuário não encontrado /:email" });
+    }
+
+    return res.status(200).json(user);
+  } catch (err) {
+    console.log(`Um erro ocorreu ao buscar usuários. ${err}`);
+    return res.status(500).json({ error: err });
+  }
+});
+
+userController.delete("/:id", auth, async (req, res) => {
+  const userId = req.params.id;
+  try {
+    // Encontra o usuário pelo ObjectId no mongo
+    const user = await UserModel.findById(userId);
+    // Se o usuário não existir:
+    if (!user) {
+      return res.status(404).json({ mensagem: "Usuário não encontrado" });
+    }
+    // Se existir: Remove o usuário
+    await UserModel.findByIdAndDelete(userId);
+    // Retorna uma mensagem de sucesso
+    return res.status(200).json({ mensagem: "Usuário deletado com sucesso" });
+  } catch (err) {
+    console.error(`Um erro ocorreu ao deletar o usuário. ${err}`);
+    return res.status(500).json({ error: err });
+  }
+});
+
+// Rota para obter todos os usuários
+userController.get("/listarUsuarios", auth, async (req, res) => {
+  try {
+    let usuarios = await UserModel.find();
+    return res.status(200).json(usuarios);
+  } catch (err) {
+    console.log(`Erro ao buscar usuários. ${err}`);
+    return res.status(500).json({ error: err });
+  }
+});
+
+// Rota para obter usuários por função
+userController.get("/usuariosPorFuncao", auth, async (req, res) => {
+  try {
+    const usuariosPorFuncao = await UserModel.aggregate([
+      {
+        $group: {
+          _id: "$funcao",
+          count: { $sum: 1 },
+        },
+      },
+    ]);
+
+    const usuariosSemFuncao = await UserModel.find({ funcao: "Não definida" });
+
+    return res.status(200).json({ usuariosPorFuncao, usuariosSemFuncao });
+  } catch (error) {
+    console.log(
+      `Erro ao buscar usuários por função e sem função definida. ${error}`
+    );
+    return res.status(500).json({ error: error });
+  }
+});
+
+const funcoes = {
+    "1": "Engenheiro de FE",
+    "2": "Engenheiro de BE",
+    "3": "Analista de dados",
+    "4": "Líder Técnico"
+  };
+  
+  userController.post("/cadastroUsuarioAutenticada", auth, async (req, res) => {
+    const { nome, email, senha, funcao } = req.body;
+  
+    const usuarioExistente = await UserModel.findOne({
+      $or: [{ nome: nome }, { email: email }],
+    });
     if (usuarioExistente) {
-        return res.status(400).json({
-            mensagem: "Nome de usuário ou email já existe!"
-        });
+      return res.status(400).json({
+        mensagem: "Nome de usuário ou email já existe!",
+      });
     }
-
+  
     const senhaEncrypt = await bcryptjs.hash(senha, 10);
-    var user = {
-        nome: nome,
-        email: email,
-        senha: senhaEncrypt,
-        funcao: "Não definida"
-    };
-
-    try {
-        await usuarioModel.create(user);
-        return res.status(201).json({
-            mensagem: "Usuário criado com sucesso!"
-        });
-    } catch(error) {
-        return res.status(500).json({
-            error: error
-        });
+    const funcaoNome = funcoes[funcao]; // Obtém o nome da função com base no número recebido
+  
+    if (!funcaoNome) {
+      return res.status(400).json({
+        mensagem: "Função inválida!",
+      });
     }
-});
-
-
-
-
-
-// Rota para obter um usuário pelo email (requer autenticação)
-userController.get("/:email", autenticado, async (req, res) => {
-    var email = req.params.email;
-
+  
+    const user = {
+      nome: nome,
+      email: email,
+      senha: senhaEncrypt,
+      funcao: funcaoNome
+    };
+  
     try {
-        let user = await usuarioModel.findOne({ email: email });
-        if(!user) {
-            return res.status(404).json({ mensagem: "Usuário não encontrado" });            
-        }
-
-        return res.status(200).json(user);
-    } catch (err) {
-        console.log(`Erro ao buscar usuários. ${err}`);
-        return res.status(500).json({ error: err });
-    } 
-});
-
-
-//Rotas autenticadas:
-
-// Rota para obter todos os usuários (requer autenticação):
-userController.get("/listarUsuarios", autenticado, async (req, res) => {
-    try {
-        let usuarios = await usuarioModel.find();
-        return res.status(200).json(usuarios);
-    } catch (err) {
-        console.log(`Erro ao buscar usuários. ${err}`);
-        return res.status(500).json({ error: err });
-    } 
-});
-
-//Rotas que possuem a const autenticado são as rotas que requerem autenticação
+      await UserModel.create(user);
+      return res.status(201).json({
+        mensagem: "Usuário criado com sucesso!",
+      });
+    } catch (error) {
+      return res.status(500).json({
+        error: error,
+      });
+    }
+  });
 
 module.exports = userController;
