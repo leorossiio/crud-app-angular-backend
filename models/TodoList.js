baa2ea2aea9cf38deec3a9012b7e4021672120b1
Leonardo Rossi <leonardo.oliveira@itixti.com.br>
2024-06-09 16:07:19 -0300

    ajusta regra do TodoList Model

--- models/TodoList.js
+++ models/TodoList.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const TodoList = mongoose.model("tarefas", {
   tarefaId: Number, //Utilizar um Random UUID para que não fique com um id aleatório do Mongo
   tarefaDescricao: String, //Descriçao sibre a task
-  usuarioAtribuido: String, //Pelo :email
+  usuarioAtribuido: String, //Pelo :idUser (imagine uma FK)
   tarefaStatus: String //Não inciado, Em andamento e Finalizado 
 });
 
