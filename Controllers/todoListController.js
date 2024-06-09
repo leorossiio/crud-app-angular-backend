e5db1621c383fb6e55e63873bacbfd3047fec54d
Leonardo Rossi <leonardo.oliveira@itixti.com.br>
2024-06-09 17:01:29 -0300

    Adiciona cadastro de tarefas

--- Controllers/todoListController.js
+++ Controllers/todoListController.js
@@ -1,24 +1,63 @@
+const bcryptjs = require("bcryptjs");
+const auth = require("../middlewares/authentication");
+const TodoList = require("../models/TodoList");
 const express = require("express");
-const todoRouter = express.Router();
-const { TodoList } = require("../models/TodoList");
-// const [ UserModel ] = require("../models/User");
+const todoListController = express.Router();
 
+function generateId() {
+    const characters = '0123456789';
+    let result = '';
+    for (let i = 0; i < 5; i++) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return result;
+}
+
+// Status de tarefa
+const statusTarefa = {
+    1: "Não iniciado",
+    2: "Em andamento",
+    3: "Concluído"
+};
 
 // Criar uma nova tarefa
+todoListController.post("/cadastroTarefas", auth, async (req, res) => {
+    const { tarefaDescricao, usuarioAtribuido, tarefa } = req.body;
 
-// Listar as tarefas do usuário logado
+    try {
+        let tarefaId = generateId();
+        let tarefaExistente = await TodoList.findOne({ tarefaId: tarefaId });
 
+        // Verificar se o tarefaId já existe e tentar novamente até encontrar um único
+        while (tarefaExistente) {
+            tarefaId = generateId();
+            tarefaExistente = await TodoList.findOne({ tarefaId: tarefaId });
+        }
 
-// Editar uma tarefa específica do usuário logado
+        const statusTarefaNome = statusTarefa[parseInt(tarefa)];
+        if (!statusTarefaNome) {
+            return res.status(400).json({
+                mensagem: "Status inválido!"
+            });
+        }
 
+        const newTask = new TodoList({
+            tarefaId: tarefaId,
+            tarefaDescricao: tarefaDescricao,
+            usuarioAtribuido: usuarioAtribuido || null,
+            tarefaStatus: statusTarefaNome
+        });
 
-// Excluir uma tarefa específica do usuário logado
+        await newTask.save();
+        return res.status(201).json({
+            mensagem: "Tarefa criada com sucesso!",
+            tarefa: newTask
+        });
+    } catch (error) {
+        return res.status(500).json({
+            error: error.message
+        });
+    }
+});
 
-
-// Tarefas sem dono
-
-
-// Adicionar um dono a uma tarefa específica
-
-
-module.exports = todoRouter;
+module.exports = todoListController;
