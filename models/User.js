9981d5876162e36d594d022a73e9eba08afeecee
Leonardo Rossi <leonardo.oliveira@itixti.com.br>
2024-06-09 13:11:45 -0300

    cria campo para IdUser

--- models/User.js
+++ models/User.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
+const { v4: uuidv4 } = require('uuid');
 
 const UserModel = mongoose.model("users", {
+  idUser: String,
   nome: String, //UserName (unico)
   email: String, //Email (unico)
-  senha: String,//Email (Podem existir senhas iguais)
+  senha: String, //Email (Podem existir senhas iguais)
   funcao: String //Funcao
 });
 
